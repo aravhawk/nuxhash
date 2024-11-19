a3274396571dddbdaeaebccf353f5c7155a0e79d
Arav Jain <aravhawk@gmail.com>
2024-11-18 21:39:23 -0600

    Workaround due to new address lengths

--- nuxhash/bitcoin.py
+++ nuxhash/bitcoin.py
@@ -2,11 +2,13 @@ __author__ = 'Rosetta Code'
 __license__ = 'GNU Free Documentation License 1.2'
 
 
+# Original code commented, in case anyone would like to replace the workaround for better address verification
+'''
 from hashlib import sha256
- 
+
 
 digits58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
- 
+
 
 def decode_base58(bc, length):
     n = 0
@@ -21,4 +23,30 @@ def check_bc(bc):
         return bcbytes[-4:] == sha256(sha256(bcbytes[:-4]).digest()).digest()[:4]
     except Exception:
         return False
+'''
 
+from hashlib import sha256
+
+digits58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+def decode_base58(bc, length):
+    n = 0
+    for char in bc:
+        n = n * 58 + digits58.index(char)
+    return n.to_bytes(length, 'big')
+
+def check_bc(bc):
+    # Check length
+    if len(bc) != 36:
+        return False
+
+    # Check if all characters are valid Base58
+    if not all(char in digits58 for char in bc):
+        return False
+
+    try:
+        # Decode address and check checksum
+        bcbytes = decode_base58(bc, 25)
+        return bcbytes[-4:] == sha256(sha256(bcbytes[:-4]).digest()).digest()[:4]
+    except Exception:
+        return False
